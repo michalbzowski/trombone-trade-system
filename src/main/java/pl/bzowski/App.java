93ee04049f1c961d9056c179b3f9e26c7049ac64
Michał Bzowski <michal.bzowski@ing.com>
2021-12-23 14:40:22 +0100

    Init commit

---
+++ src/main/java/pl/bzowski/App.java
@@ -0,0 +1,13 @@
+package pl.bzowski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
